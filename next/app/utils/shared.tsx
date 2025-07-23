4231c0aa658a18bf357f53c36308d42967845498
markhobbs <hobbs1m@hotmail.com>
2025-07-22 22:54:31 -0700

    simplifying friendly amount

--- next/app/utils/shared.tsx
+++ next/app/utils/shared.tsx
@@ -5,22 +5,19 @@ export const capitalizeFirst = (str: string) => {
 
 export const convertAmountFriendly = (quantity: string) => {
   let response;
-  const intQuantity = (parseInt(quantity) !== 0) && `${parseInt(quantity)} `;
-  const decimalPart = quantity.slice(-3);
-  switch (decimalPart) {
-    case ".00":
-      response = "1";
+  const float = parseFloat(quantity)
+  switch (float) {
+    case 0.25:
+      response = "1/4";
       break;
-    case ".25":
-      response = intQuantity + "1/4";
+    case 0.50:
+      response = "1/2";
       break;
-    case ".50":
-      response = intQuantity + "1/2";
-      break;
-    case ".75":
-      response = intQuantity + "3/4";
+    case 0.75:
+      response = "3/4";
       break;
     default:
+      response = "1";
       break;
   }
   return response;
